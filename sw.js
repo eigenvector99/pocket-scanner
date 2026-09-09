ed96d9b0bc9ca36977fa10ffc538c76565e2245b
eigenvector99 <hskim5851@gmail.com>
2026-09-09 23:25:00 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,50 @@
+// 주머니 스캐너 서비스워커
+// 파일을 고쳐서 올릴 때마다 CACHE 뒤 숫자를 올리면 아이폰이 새 버전을 받아갑니다.
+const CACHE = 'scanner-v1';
+
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './apple-touch-icon.png',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE)
+      .then((c) => c.addAll(ASSETS))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+// 캐시 우선 + 뒤에서 조용히 갱신 (오프라인에서 그대로 실행됨)
+self.addEventListener('fetch', (e) => {
+  const req = e.request;
+  if (req.method !== 'GET') return;
+  if (new URL(req.url).origin !== location.origin) return;
+
+  e.respondWith(
+    caches.match(req).then((hit) => {
+      const net = fetch(req)
+        .then((res) => {
+          if (res && res.ok) {
+            const copy = res.clone();
+            caches.open(CACHE).then((c) => c.put(req, copy));
+          }
+          return res;
+        })
+        .catch(() => hit || caches.match('./index.html'));
+      return hit || net;
+    })
+  );
+});
